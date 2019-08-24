aabb78e000dc3c70732313fcf690f13568f3d8df
Eyeball Edward <edwardyeung.private@gmail.com>
2019-08-24 11:13:45 +1000

    include tracker; have plans to track schbus on weekdays too

--- backend-tracker.js
+++ backend-tracker.js
@@ -3,7 +3,7 @@ const WebsocketTracker = require('./trackers/WebsocketTracker')
 const urlData = require('./url_data.json')
 
 Object.keys(urlData).forEach(service => {
-  if (service <= 982 || service.includes('Telebus')) {
+  if (service <= 982 || service.includes('Telebus') || service === 'Point Nepean Shuttle') {
     let url = urlData[service]
     if (url.includes('/live/'))
       new WebsocketTracker(service).start()
