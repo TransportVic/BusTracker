dff09d046dfdce19690f8a4f60f324a66847056b
Eyeball Edward <edwardyeung.private@gmail.com>
2019-09-16 18:04:30 +1000

    slow down?

--- backend-tracker.js
+++ backend-tracker.js
@@ -113,8 +113,8 @@ function checkTrackers() {
     let newState = runsToday && runsNow
 
     if (newState && !tracker.running) {
-      const delay = (Math.random() * (5) + 1.5 * i) * 500;
-      console.log('delaying tracker for ' + tracker.service + ' by ' + (Math.random() * (5) + 1.5 * i) * 500/1000 + ' seconds')
+      const delay = (Math.random() * (10) + 2.5 * i) * 1000;
+      console.log('delaying tracker for ' + tracker.service + ' by ' + (Math.random() * (10) + 2.5 * i) * 1000/1000 + ' seconds')
       setTimeout(() => {
         console.log('activated tracker for ' + tracker.service)
         tracker.tracker.start()
